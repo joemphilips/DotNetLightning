e9784cb129ba574af4d060405be4b0a5327e9f29
joemphilips <joemphilips@gmail.com>
2019-06-08 00:47:34 +0900

    Update LightningStream

--- src/DotNetLightning/Serialize/LightningStream.fs
+++ src/DotNetLightning/Serialize/LightningStream.fs
@@ -2,11 +2,9 @@ namespace DotNetLightning.Serialize
 
 open System
 open System.IO
-open System.IO.Pipelines
-open System.Threading
+open System.Runtime.Serialization
 open DotNetLightning.Utils
 open DotNetLightning.Utils.NBitcoinExtensions
-open DotNetLightning.Serialize.Msgs
 open NBitcoin
 open NBitcoin.Crypto
 
@@ -20,16 +18,14 @@ type Scope(openAction: Action, closeAction: Action) =
 /// Simple Wrapper stream for serializing Lightning Network p2p messages.
 /// Why not use BitWriter/Reader? Because it does not support big endian and
 /// We might want to extend it in the future.
-type LightningStream(inner: Stream) =
+[<AbstractClass>]
+type LightningStream(inner: Stream) = 
     inherit Stream()
     let _inner = inner
     member this.Inner = _inner
 
-    member val MaxArraySize = 1024*1024
 
-    override this.CanRead = this.Inner.CanRead
     override this.CanSeek = this.Inner.CanSeek
-    override this.CanWrite = this.Inner.CanWrite
     override this.Flush() = this.Inner.Flush()
     override this.Length = this.Inner.Length
     override this.Position
@@ -39,14 +35,35 @@ type LightningStream(inner: Stream) =
         this.Inner.Seek(offset, origin)
     override this.SetLength(value: int64) =
         raise (NotSupportedException("SetLength is not supported for LigningStream"))
+    override this.Close() =
+        this.Inner.Close()
 
-    override this.Read(buf: byte[], offset: int, count: int) = 
-        this.Inner.Read(buf, offset, count)
     override this.Write(buffer: byte[], offset: int, count: int) =
         this.Inner.Write(buffer, offset, count)
     
     member this.Write(buffer: byte[]) =
         this.Write(buffer, 0 , buffer.Length)
+
+    member this.Write(b: byte) =
+        this.WriteByte(b)
+
+    override this.Read(buf: byte[], offset: int, count: int) = 
+        this.Inner.Read(buf, offset, count)
+type LightningWriterStream(inner: Stream) =
+    inherit LightningStream(inner)
+
+    do if not (inner.CanWrite) then invalidArg "inner" "inner stream must be writable"
+    override this.CanWrite = true
+    override this.CanRead = this.Inner.CanRead
+
+    override this.Write(buffer: byte[], offset: int, count: int) =
+        this.Inner.Write(buffer, offset, count)
+    
+    override this.Write(buffer: ReadOnlySpan<byte>) =
+        this.Inner.Write(buffer)
+
+    member this.Write(buf: byte[]) =
+        this.Write(buf, 0, buf.Length)
 
     member this.Write(b: byte) =
         this.WriteByte(b)
@@ -138,30 +155,141 @@ type LightningStream(inner: Stream) =
         | Some d -> this.WriteWithLen(d)
         | None -> ()
 
-    member this.Write(data: NetAddress) =
-        match data with
-        | IPv4 d ->
-            this.Write(d.Address.GetAddressBytes())
-            this.Write((uint16)d.Port, false)
-        | IPv6 d ->
-            this.Write(d.Address.GetAddressBytes())
-            this.Write((uint16)d.Port, false)
-        | OnionV2 d ->
-            this.Write(d.Addr)
-            this.Write((uint16)d.Port, false)
-        | OnionV3 d ->
-            this.Write(d.ed25519PubKey)
-            this.Write(d.CheckSum, false)
-            this.Write(d.Version)
-            this.Write((uint16)d.Port, false)
 
-    member this.Write(data: DateTime) =
-        failwith "not impl"
+type LightningReaderStream(inner: Stream) =
+    inherit LightningStream(inner)
+    do if (not inner.CanRead) then invalidArg "inner" "inner stream must be readable"
+    let MaxArraySize = 1024*1024
+    let mutable m_buffer = Array.zeroCreate MaxArraySize
 
-    member this.Write(data: OnionPacket) =
-        this.WriteByte(data.Version)
-        this.Write(data.PublicKey.ToBytes())
-        this.Write(data.HopData)
-        this.Write(data.HMAC.ToBytes())
-    override this.Close() =
-        this.Inner.Close()
+    override this.CanRead = true
+    override this.CanWrite = this.Inner.CanWrite
+
+    member private this.FillBuffer(numBytes: int) =
+        if (isNull m_buffer) then
+            failwith "m_buffer was null"
+        if (numBytes < 0 || (numBytes > m_buffer.Length)) then
+            raise (ArgumentOutOfRangeException(sprintf "numBytes was %d" numBytes))
+
+        let mutable n = 0
+        if (numBytes = 1) then
+            n <- this.Inner.ReadByte()
+            if (n = -1) then
+                raise (EndOfStreamException "Inner Stream for LightningReaderStream has been consumed")
+            m_buffer.[0] <- byte n
+        else
+            let mutable bytesRead = 0
+            while (bytesRead < numBytes) do
+                n <- this.Inner.Read(m_buffer, bytesRead, numBytes - bytesRead)
+                if (n = 0) then
+                    raise (EndOfStreamException "Inner Stream for LightningReaderStream has been consumed")
+                bytesRead <- bytesRead + n
+
+    // ##### Methods for reading
+    member this.ReadBoolean() =
+        this.FillBuffer(1)
+        m_buffer.[0] <> 0uy
+
+    member this.ReadByte(): byte =
+        let b = this.Inner.ReadByte()
+        if (b = -1) then
+            raise (EndOfStreamException "Inner Stream for LightningReaderStream has been comsumed")
+        else
+            (byte b)
+
+    member this.ReadUInt8() =
+        uint8 (this.ReadByte())
+    
+    member this.ReadSByte() =
+        this.FillBuffer(1)
+        (sbyte m_buffer.[0])
+
+    member this.ReadInt16(lendian: bool) =
+        this.FillBuffer(2)
+        if lendian then
+            ((int16 m_buffer.[0]) ||| int16 (m_buffer.[1] <<< 8))
+        else
+            ((int16 m_buffer.[0]) <<< 8 ||| int16 (m_buffer.[1]))
+
+    member this.ReadUInt16(lendian: bool) =
+        this.FillBuffer(2)
+        if lendian then
+            ((uint16 m_buffer.[0]) ||| uint16 (m_buffer.[1] <<< 8))
+        else
+            ((uint16 m_buffer.[0]) <<< 8 ||| uint16 (m_buffer.[1]))
+
+    member this.ReadInt32(lendian: bool) =
+        this.FillBuffer(4)
+        if lendian then
+            ((int32 m_buffer.[0]) |||
+                int32 (m_buffer.[1] <<< 8) |||
+                int32 (m_buffer.[2] <<< 16) |||
+                int32 (m_buffer.[3] <<< 24))
+        else
+            ((int32 m_buffer.[0]) <<< 24 |||
+                int32 (m_buffer.[1] <<< 16) |||
+                int32 (m_buffer.[2] <<< 8) |||
+                int32 m_buffer.[3])
+
+    member this.ReadUInt32(lendian: bool) =
+        this.FillBuffer(4)
+        if lendian then
+            ((uint32 m_buffer.[0]) ||| uint32 (m_buffer.[1] <<< 8) ||| uint32 (m_buffer.[2] <<< 16) ||| uint32 (m_buffer.[3] <<< 24))
+        else
+            ((uint32 m_buffer.[0]) <<< 24 ||| uint32 (m_buffer.[1] <<< 16) ||| (uint32 (m_buffer.[2]) <<< 8) ||| (uint32 (m_buffer.[3])))
+    member this.ReadInt64(lendian: bool) =
+        this.FillBuffer(8)
+        if lendian then
+            ((int64 m_buffer.[0]) |||
+                int64 (m_buffer.[1] <<< 8) |||
+                int64 (m_buffer.[2] <<< 16) |||
+                int64 (m_buffer.[3] <<< 24) |||
+                int64 (m_buffer.[4] <<< 32) |||
+                int64 (m_buffer.[5] <<< 40) |||
+                int64 (m_buffer.[6] <<< 48) |||
+                int64 (m_buffer.[7] <<< 56))
+        else
+            ((int64 m_buffer.[0]) <<< 56 |||
+                int64 (m_buffer.[1] <<< 48) |||
+                int64 (m_buffer.[2] <<< 40) |||
+                int64 (m_buffer.[3] <<< 32) |||
+                int64 (m_buffer.[4] <<< 24) |||
+                int64 (m_buffer.[5] <<< 16) |||
+                int64 (m_buffer.[6] <<< 8) |||
+                int64 m_buffer.[7])
+
+    member this.ReadUInt64(lendian: bool) =
+        this.FillBuffer(8)
+        if lendian then
+            ((uint64 m_buffer.[0]) |||
+                uint64 (m_buffer.[1] <<< 8) |||
+                uint64 (m_buffer.[2] <<< 16) |||
+                uint64 (m_buffer.[3] <<< 24) |||
+                uint64 (m_buffer.[4] <<< 32) |||
+                uint64 (m_buffer.[5] <<< 40) |||
+                uint64 (m_buffer.[6] <<< 48) |||
+                uint64 (m_buffer.[7] <<< 56))
+        else
+            ((uint64 m_buffer.[0]) <<< 56 |||
+                uint64 (m_buffer.[1] <<< 48) |||
+                uint64 (m_buffer.[2] <<< 40) |||
+                uint64 (m_buffer.[3] <<< 32) |||
+                uint64 (m_buffer.[4] <<< 24) |||
+                uint64 (m_buffer.[5] <<< 16) |||
+                uint64 (m_buffer.[6] <<< 8) |||
+                uint64 m_buffer.[7])
+
+    member this.ReadUInt256(lendian: bool) =
+        let b = this.ReadBytes(32)
+        uint256(b, lendian)
+
+    member this.ReadWithLen() =
+        let len = this.ReadUInt16(false)
+        this.ReadBytes(int32 len)
+
+    member this.ReadPubKey() =
+        let b = this.ReadBytes(33)
+        if PubKey.Check(b, true) then
+            PubKey(b, true)
+        else
+            raise (SerializationException("Invalid Pubkey encoding"))
