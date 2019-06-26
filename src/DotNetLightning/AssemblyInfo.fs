bf5ca678e408bd09a68c05faea284d051cc6b007
joemphilips <joemphilips@gmail.com>
2019-06-26 17:56:51 +0900

    Make channel ADT and set visibility to 'internal'

---
+++ src/DotNetLightning/AssemblyInfo.fs
@@ -0,0 +1,36 @@
+namespace System
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitleAttribute("DotNetLightning")>]
+[<assembly:AssemblyProductAttribute("DotNetLightning")>]
+[<assembly:AssemblyVersionAttribute("0.1.0")>]
+[<assembly:AssemblyFileVersionAttribute("0.1.0")>]
+[<assembly:AssemblyInformationalVersionAttribute("0.1.0")>]
+[<assembly:AssemblyMetadataAttribute("ReleaseChannel", "release")>]
+[<assembly:AssemblyDescriptionAttribute("Lightning Network Daemon for DotNet")>]
+[<assembly: InternalsVisibleTo("DotNetLightning.Tests")>]
+[<assembly: InternalsVisibleTo("DotNetLightning.Tests")>]
+
+do ()
+
+module internal AssemblyVersionInformation =
+    [<Literal>]
+    let AssemblyTitle = "DotNetLightning"
+
+    [<Literal>]
+    let AssemblyProduct = "DotNetLightning"
+
+    [<Literal>]
+    let AssemblyVersion = "0.1.0"
+
+    [<Literal>]
+    let AssemblyFileVersion = "0.1.0"
+
+    [<Literal>]
+    let AssemblyInformationalVersion = "0.1.0"
+
+    [<Literal>]
+    let AssemblyMetadata_ReleaseChannel = "release"
+
