703fdd0fbe30e5839cdd3ca9fad2d8c76860c9ae
joemphilips <joemphilips@gmail.com>
2019-07-11 08:32:00 +0900

    fix bug in buildCommitmentSecret

--- src/DotNetLightning/LN/ChannelUtils.fs
+++ src/DotNetLightning/LN/ChannelUtils.fs
@@ -69,7 +69,7 @@ module ChannelUtils =
     /// refs: https://github.com/rustyrussell/ccan/blob/master/ccan/crypto/shachain/design.txt
     let buildCommitmentSecret (commitmentSeed: uint256, index: uint64): uint256 =
         let mutable res = commitmentSeed.ToBytes()
-        for i in 0..48 do
+        for i in 0..47 do
             let bitpos = 47 - i
             if index &&& (1UL <<< bitpos) = (1UL <<< bitpos) then
                 res.[bitpos / 8] <- (res.[bitpos / 8] ^^^ (1uy <<< (7 &&& bitpos)))
