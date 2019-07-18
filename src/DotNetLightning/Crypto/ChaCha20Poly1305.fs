3758b895a04b36134e8e09686dc24ea30cf40ca5
joemphilips <joemphilips@gmail.com>
2019-07-18 18:12:49 +0900

    Delete uselss ChaCha20Poly1305.fs

--- src/DotNetLightning/Crypto/ChaCha20Poly1305.fs
+++
@@ -1,7 +0,0 @@
-namespace DotNetLightning.Crypto
-
-/// Custom ChaCha20-Poly1305
-/// This might be replaced when corefx includes the algorithm
-/// refs: https://github.com/dotnet/corefx/issues/5162
-
-
