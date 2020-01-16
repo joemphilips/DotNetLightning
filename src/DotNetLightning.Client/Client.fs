11ceb82da596754748f19199f3da7c3cb7f3f59f
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2020-01-16 17:08:07 +0900

    WIP: write DotNetLightning.Client

---
+++ src/DotNetLightning.Client/Client.fs
@@ -0,0 +1,27 @@
+namespace DotNetLightning.Server.Client
+
+open FSharp.Control.Tasks
+open System.Threading.Tasks
+open System.Text.Json
+
+open System.Net.Http
+
+type Client() =
+    let _client = new HttpClient()
+    member this.GetNewAddress() = task {
+        let! response = _client.GetAsync("/api/v1/address")
+        let mutable address = null
+        if (response.IsSuccessStatusCode) then
+            let! content = response.Content.ReadAsStringAsync()
+            address <- content
+        return address
+    }
+        
+    member this.SendAsync(method: HttpMethod, body: obj, relativePath:string, parameters: obj[]) = task {
+        let httpRequestMessage = this.CreateMessage()
+        let! result = _client.SendAsync(message)
+        return failwith ""
+    }
+    
+    member this.CreateMessage() =
+        failwith ""
