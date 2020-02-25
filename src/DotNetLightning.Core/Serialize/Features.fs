181dd478fbde2013a474d0314708ddea614cccd1
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2020-02-25 17:53:07 +0900

    Prepare new type `Feature` to represent featurebits

---
+++ src/DotNetLightning.Core/Serialize/Features.fs
@@ -0,0 +1,122 @@
+namespace DotNetLightning.Core.Serialize
+
+open System.Collections
+
+open ResultUtils
+
+open DotNetLightning.Core.Utils.Extensions
+
+type FeaturesSupport =
+    | Mandatory
+    | Optional
+
+/// Feature bits specified in BOLT 9.
+/// It has no constructors, use its static members to instantiate
+type Feature = private {
+    RfcName: string
+    Mandatory: int
+}
+    with
+    member this.MandatoryBitPosition = this.Mandatory
+    member this.OptionalBitPosition = this.Mandatory + 1
+    override this.ToString() = this.RfcName
+
+    static member OptionDataLossProtect = {
+        RfcName = "option_data_loss_protect"
+        Mandatory = 0
+    }
+        
+    static member InitialRoutingSync = {
+        RfcName = "initial_routing_sync"
+        Mandatory = 2
+    }
+    
+    static member ChannelRangeQueries = {
+        RfcName = "gossip_queries"
+        Mandatory = 6
+    }
+    
+    static member VariableLengthOnion = {
+        RfcName = "var_onion_optin"
+        Mandatory = 8
+    }
+    
+    static member ChannelRangeQueriesExtended = {
+        RfcName = "gossip_queries_ex"
+        Mandatory = 10
+    }
+    
+    static member PaymentSecret = {
+        RfcName = "payment_secret"
+        Mandatory = 14
+    }
+    
+    static member BasicMultiPartPayment = {
+        RfcName = "basic_mpp"
+        Mandatory = 16
+    }
+    
+module Feature =
+    /// Features may depend on other features, as specified in BOLT 9
+    let private featuresDependency =
+        Map.empty
+        |> Map.add (Feature.ChannelRangeQueriesExtended) ([Feature.ChannelRangeQueries])
+        |> Map.add (Feature.BasicMultiPartPayment) ([Feature.PaymentSecret])
+        
+    let private isFeatureOn(features: BitArray) (bit: int) =
+        (features.Length > bit) && features.Reverse().[bit]
+        
+    let hasFeature(features: BitArray) (f: Feature) (support: FeaturesSupport option) =
+        match support with
+        | Some(Mandatory) ->
+            isFeatureOn(features) (f.Mandatory)
+        | Some(Optional) ->
+            isFeatureOn(features) (f.OptionalBitPosition)
+        | None ->
+            isFeatureOn(features) (f.OptionalBitPosition) || isFeatureOn(features) (f.Mandatory)
+        
+    let private printDeps (deps: #seq<Feature>) (features) =
+        deps
+        |> Seq.filter(fun d -> not <| (hasFeature(features) (d) (None) ))
+        |> Seq.map(fun d -> d.ToString())
+        |> String.concat " and "
+    let validateFeatureGraph(features: BitArray) =
+        result {
+            let mutable c = 0
+            for kvp in featuresDependency do
+                c <- c + 1
+                let f = kvp.Key
+                let deps = kvp.Value
+                if hasFeature(features) (f) (None) && deps |> List.exists(fun d -> not <| hasFeature(features) (d) (None)) then
+                    return!
+                        sprintf
+                            "%s sets %s but is missing a dependency %s "
+                            (features.PrintBits())
+                            (f.ToString())
+                            (printDeps deps features)
+                        |> Error
+                else
+                    return ()
+        }
+        
+    /// Check that the features that we understand are correctly specified, and that there are no mandatory features
+    /// we don't understand
+    let areSupported(features: BitArray) =
+        let supportedMandatoryFeatures =
+            seq { Feature.OptionDataLossProtect
+                  Feature.ChannelRangeQueries
+                  Feature.VariableLengthOnion
+                  Feature.ChannelRangeQueriesExtended
+                  Feature.PaymentSecret
+                  Feature.BasicMultiPartPayment }
+            |> Seq.map(fun f -> f.Mandatory)
+            |> Set
+            
+        let reversed = features.Reverse()
+        seq {
+            for i in 0..reversed.Length - 1 do
+                if (i % 2 = 0) then
+                    yield i
+        }
+        |> Seq.exists(fun i -> reversed.[i] && not <| supportedMandatoryFeatures.Contains(i))
+        |> not
--- src/DotNetLightning.Core/Utils/Extensions.fs
+++ src/DotNetLightning.Core/Utils/Extensions.fs
@@ -4,6 +4,7 @@ open System
 open System.Collections
 open System.Collections.Generic
 open System.Runtime.CompilerServices
+open System.Text
 
 module Dict =
     let tryGetValue key (dict: IDictionary<_,_>)=
@@ -22,7 +23,7 @@ type System.UInt16 with
     member this.GetBytesBigEndian() =
         let d = BitConverter.GetBytes(this)
         if BitConverter.IsLittleEndian then (d |> Array.rev) else d
-        
+    
 type System.Collections.BitArray with
     member this.ToByteArray() =
         let ret: byte[] = Array.zeroCreate (((this.Length - 1) / 8) + 1)
@@ -38,13 +39,47 @@ type System.Collections.BitArray with
             bitArray.Set(di * 5 + 3, ((b.[di] >>> 1) &&& 0x01uy) = 1uy)
             bitArray.Set(di * 5 + 4, ((b.[di] >>> 0) &&& 0x01uy) = 1uy)
         bitArray
-        
     static member FromUInt32(d: uint32) =
         let b = d.GetBytesBigEndian()
         BitArray.From5BitEncoding(b)
         
+    member this.Reverse() =
+        let length = this.Length
+        let mutable result = Array.zeroCreate this.Length
+        let mid = length / 2
+        for i in 0..mid - 1 do
+            let bit = this.[i]
+            result.[i] <- this.[length - i - 1]
+            result.[length - i - 1] <- bit
+        result
+    member this.PrintBits() =
+        let sb = StringBuilder()
+        for b in this do
+            (if b then "1" else "0") |> sb.Append |> ignore
+        sb.ToString()
+        
+    static member FromInt64(value: int64) =
+        let mutable v = value
+        let array = Array.zeroCreate 64
+        for i in 0..(64 - 1) do
+            array.[i] <- (v &&& 1L) = 1L
+            v <- v >>> 1
+        BitArray(array |> Array.rev)
+        
+    static member Parse(str: string) =
+        let mutable str = str.Trim().Clone() :?> string
+        if str.StartsWith("0b", StringComparison.OrdinalIgnoreCase) then
+            str <- str.Substring("0b".Length)
+        let array = Array.zeroCreate(str.Length)
+        for i in 0..str.Length - 1 do
+            array.[i] <-
+                if str.[i] = '0' then false else
+                if str.[i] = '1' then true else
+                raise <| FormatException(sprintf "Failed to parse BitArray! it must have only '0' or '1' but we found %A" str.[i])
+        array |> BitArray
+        
     
-[<Extension>]
+[<Extension;AbstractClass;Sealed>]
 type DictionaryExtensions() =
 
     [<Extension>]
