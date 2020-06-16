ef94089cb3493cfa1cc67bfd03f536b621d94ff4
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2020-06-16 11:17:22 +0900

    remvoe unused test framework from solution

--- tests/DotNetLightning.TestFramework/Utils.fs
+++
@@ -1,8 +0,0 @@
-[<AutoOpen>]
-module internal DotNetLightning.TestFramework.Utils
-
-open System.IO
-open System.Reflection
-
-let getAssemblyDirectory () =
-    Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) |> Path.GetFullPath
