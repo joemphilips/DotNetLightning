daad1dc36948987763df727056c0340349ec8f28
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2020-09-03 12:15:15 +0900

    include FastBlake2b into the solution

---
+++ src/Blake2Fast/Blake2Hmac.cs
@@ -0,0 +1,71 @@
+﻿#if BLAKE2_CRYPTOGRAPHY
+using System;
+using System.Security.Cryptography;
+
+namespace Blake2Fast
+{
+	internal sealed class Blake2Hmac : HMAC
+	{
+		internal enum Algorithm
+		{
+			Blake2b,
+			Blake2s
+		}
+
+#if NETSTANDARD1_3
+		private readonly int HashSizeValue;
+		public sealed override int HashSize => HashSizeValue;
+#endif
+
+		private readonly Algorithm alg;
+		private IBlake2Incremental impl;
+
+		public Blake2Hmac(Algorithm hashAlg, int hashBytes, ReadOnlySpan<byte> key)
+		{
+			alg = hashAlg;
+			HashSizeValue = hashBytes * 8;
+			HashName = alg.ToString();
+
+			if (key.Length > 0)
+#if NETSTANDARD1_3
+				base.Key = key.ToArray();
+#else
+				KeyValue = key.ToArray();
+#endif
+
+			impl = createIncrementalInstance();
+		}
+
+		public override byte[] Key
+		{
+			get => base.Key;
+			set => throw new InvalidOperationException($"{nameof(Key)} cannot be changed.  You must create a new {nameof(HMAC)} instance.");
+		}
+
+		public sealed override void Initialize() => impl = createIncrementalInstance();
+
+		protected sealed override void HashCore(byte[] array, int ibStart, int cbSize) => impl.Update(new ReadOnlySpan<byte>(array, ibStart, cbSize));
+
+		protected sealed override byte[] HashFinal() => impl.Finish();
+
+#if BUILTIN_SPAN
+		protected sealed override void HashCore(ReadOnlySpan<byte> source) => impl.Update(source);
+
+		protected sealed override bool TryHashFinal(Span<byte> destination, out int bytesWritten) => impl.TryFinish(destination, out bytesWritten);
+#endif
+
+		private IBlake2Incremental createIncrementalInstance()
+		{
+#if NETSTANDARD1_3
+			var key = new ReadOnlySpan<byte>(base.Key);
+#else
+			var key = new ReadOnlySpan<byte>(KeyValue);
+#endif
+
+			return alg == Algorithm.Blake2b ?
+				(IBlake2Incremental)Blake2b.CreateIncrementalHasher(HashSizeValue / 8, key) :
+				(IBlake2Incremental)Blake2s.CreateIncrementalHasher(HashSizeValue / 8, key);
+		}
+	}
+}
+#endif
---
+++ src/Blake2Fast/ThrowHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace Blake2Fast
+{
+	internal static class ThrowHelper
+	{
+#if !BUILTIN_SPAN
+		private static class TypeCache<T>
+		{
+			internal static readonly bool IsReferenceOrContainsReferences = checkRefs(typeof(T));
+
+			private static bool checkRefs(Type t)
+			{
+				if (t.IsPointer)
+					return false;
+
+				var ti = t.GetTypeInfo();
+				if (!ti.IsValueType)
+					return true;
+
+				return ti.DeclaredFields.Any(fi => !fi.IsStatic && fi.FieldType != t && checkRefs(fi.FieldType));
+			}
+		}
+#endif
+
+		public static void ThrowIfIsRefOrContainsRefs<T>()
+		{
+			if (
+#if BUILTIN_SPAN
+				RuntimeHelpers.IsReferenceOrContainsReferences<T>()
+#else
+				TypeCache<T>.IsReferenceOrContainsReferences
+#endif
+			) throw new NotSupportedException("This method may only be used with value types that do not contain reference type fields.");
+		}
+
+		public static void HashFinalized() => throw new InvalidOperationException("Hash has already been finalized.");
+
+		public static void HashNotInitialized() => throw new InvalidOperationException("Hash not initialized.  Do not create the state struct instance directly; use CreateIncrementalHasher.");
+
+		public static void NoBigEndian() => throw new PlatformNotSupportedException("Big-endian platforms not supported");
+
+		public static void DigestInvalidLength(int max) => throw new ArgumentOutOfRangeException("digestLength", $"Value must be between 1 and {max}");
+
+		public static void KeyTooLong(int max) => throw new ArgumentException($"Key must be between 0 and {max} bytes in length", "key");
+
+		public static void OutputTooSmall(int min) => throw new ArgumentException($"Output must be at least {min} bytes in length", "output");
+	}
+}
