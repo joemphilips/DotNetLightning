762652a1f26b90328c90f71e8b46aeada6e5f1d6
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2020-09-07 20:49:11 +0900

    remove server and its tests

--- src/DotNetLightning.Server/P2PConnectionMiddleware.fs
+++
@@ -1,7 +0,0 @@
-namespace DotNetLightning.Server.P2PConnectionMiddleware
-
-open Microsoft.AspNetCore.Connections
-open Microsoft.Extensions.Logging
-
-type P2PConnectionMiddleware(next: ConnectionDelegate, loggerFactory: ILoggerFactory) =
-    let logger = loggerFactory.CreateLogger<P2PConnectionMiddleware>()
