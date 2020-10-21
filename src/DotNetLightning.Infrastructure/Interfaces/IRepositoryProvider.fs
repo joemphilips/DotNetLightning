b8945895fa5fee68cc3549e2b194e4cb68ab77c1
Andres G. Aragoneses <knocte@gmail.com>
2020-10-21 11:57:21 +0200

    Remove Infrastructure(.*)
    Fixes https://github.com/joemphilips/DotNetLightning/issues/98

--- src/DotNetLightning.Infrastructure/Interfaces/IRepositoryProvider.fs
+++
@@ -1,8 +0,0 @@
-namespace DotNetLightning.Infrastructure.Interfaces
-
-open DotNetLightning.Infrastructure
-
-type IRepositoryProvider =
-    abstract member TryGetRepository: DotNetLightningNetwork -> IRepository option
-
-
