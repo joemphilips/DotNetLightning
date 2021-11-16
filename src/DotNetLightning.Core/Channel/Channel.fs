aarani/DotNetLightning.Kiss

Add support for option_support_large_channel
This commit adds support for option_support_large_channel
and there are some changes made to validation process;
more specifically when validating our OpenChannel msg,
instead of just sending out an Error if the funding_satoshis
is greater than the limit, we check if support_large_channel
is activated as mandatory (cause if we don't set that option as
mandatory the openChannel will be invalid if sent to a peet with
no support for support_large_channel) and also when we are validating
someone else's openChannelMsg, we check that we have
support_large_channel option activated (it doesn't matter if mandatory
or optional).

src/DotNetLightning.Core/Channel/Channel.fs
@@ -408,7 +408,7 @@ and Channel = {
             TLVs = [| OpenChannelTLV.UpfrontShutdownScript shutdownScriptPubKey |]
         }
         result {
-            do! Validation.checkOurOpenChannelMsgAcceptable openChannelMsgToSend
+            do! Validation.checkOurOpenChannelMsgAcceptable openChannelMsgToSend localParams
             let channelPrivKeys = nodeMasterPrivKey.ChannelPrivKeys channelIndex
             let nodeSecret = nodeMasterPrivKey.NodeSecret()
             let channelWaitingForAcceptChannel = {
@@ -450,6 +450,7 @@ and Channel = {
                     channelOptions
                     announceChannel
                     openChannelMsg
+                    localParams
             let firstPerCommitmentPoint = channelPrivKeys.CommitmentSeed.DerivePerCommitmentPoint CommitmentNumber.FirstCommitment
             let acceptChannelMsg: AcceptChannelMsg = {
                 TemporaryChannelId = openChannelMsg.TemporaryChannelId

joemphilips/DotNetLightning

src/DotNetLightning.Core/Channel/Channel.fs
@@ -408,7 +408,7 @@ and Channel = {
             TLVs = [| OpenChannelTLV.UpfrontShutdownScript shutdownScriptPubKey |]
         }
         result {
-            do! Validation.checkOurOpenChannelMsgAcceptable openChannelMsgToSend
+            do! Validation.checkOurOpenChannelMsgAcceptable localParams openChannelMsgToSend
             let channelPrivKeys = nodeMasterPrivKey.ChannelPrivKeys channelIndex
             let nodeSecret = nodeMasterPrivKey.NodeSecret()
             let channelWaitingForAcceptChannel = {
@@ -449,6 +449,7 @@ and Channel = {
                     channelHandshakeLimits
                     channelOptions
                     announceChannel
+                    localParams
                     openChannelMsg
             let firstPerCommitmentPoint = channelPrivKeys.CommitmentSeed.DerivePerCommitmentPoint CommitmentNumber.FirstCommitment
             let acceptChannelMsg: AcceptChannelMsg = {