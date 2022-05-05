aaa7109a67b9e0cabedc38fa9cce34e94f3aa028
joemphilips <joe.miyamoto@bitcoinbank.co.jp>
2022-05-05 15:52:05 +0900

    add top-level document for DNL.Payment.LSAT

--- src/DotNetLightning.Core/Payment/LSAT/Constants.fs
+++ src/DotNetLightning.Core/Payment/LSAT/Constants.fs
@@ -1,5 +1,13 @@
 namespace DotNetLightning.Payment.LSAT
 
+
+/// <namespacedoc>
+///     <summary>
+///         "DotNetLightning.Payment.LSAT" contains a utility to work with
+///         Lightning Service Authentication Token (LSAT).
+///         Which is a wrapper around Macaroons.
+///     </summary>
+/// </namespacedoc>
 module Constants =
     [<Literal>]
     let CAPABILITIES_CONDITION_PREFIX = "_capabilities"
